eb816fa7b1e7713790e09ee344090f34aab081e7
dotie-spec <121659073+dotie-spec@users.noreply.github.com>
2023-11-16 23:43:22 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+function getStarted () {
+    let name = 
+}
+let mainButton = document.querySelector(".main__btn");
+mainButton.addEventListener("click, getStarted");
